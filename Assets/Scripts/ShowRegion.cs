03ad98cb349960884b2ba73e3f6f84d359ee7d1c
36yang <948082928@QQ.COM>
2017-07-27 16:54:14 +0800

    完善导出工具
    完善导出工具

---
+++ Assets/Scripts/ShowRegion.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class ShowRegion : MonoBehaviour {
+
+
+    public GameObject wallPrefab = null;
+
+    public  bool regionEnabled = false;
+    public  bool showRegion = false;
+    public  Rect sceneRegion = new Rect(-250.0f, -250.0f, 500.0f, 500.0f);
+    public  int gridX = 1;
+    public  int gridY = 1;
+
+    private Rect mSceneRect = new Rect(-250.0f, -250.0f, 500.0f, 500.0f);
+    private int X = 1;
+    private int Y = 1;
+    GameObject mObjectGroup = null;
+
+   
+  
+	// Use this for initialization
+	void Start () 
+    {
+     
+        EditorApplication.update += MyUpdate;    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+
+    void MyUpdate()
+    {
+       
+        if(showRegion)
+        {
+            if(mObjectGroup == null)
+            {
+
+                mObjectGroup = new GameObject("showRegionGroup");
+                GameObject XObjs = new GameObject("XObjs");
+                GameObject YObjs = new GameObject("YObjs");
+                XObjs.transform.parent = mObjectGroup.transform;
+                YObjs.transform.parent = mObjectGroup.transform;
+            
+
+                float xSkip = mSceneRect.width / X;
+                float ySkip = mSceneRect.height / Y;
+
+                for(int i=0; i<= X; i++)
+                {
+                    //GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    
+                    GameObject go = Instantiate(wallPrefab);
+                    go.transform.localScale = new Vector3(mSceneRect.height, 20, 0.01f);
+                    go.transform.Rotate(new Vector3(0, 90, 0));
+                    go.transform.position = new Vector3(mSceneRect.x + i * xSkip, 10, mSceneRect.y + mSceneRect.height / 2.0f);
+                    go.transform.parent = XObjs.transform;
+                }
+
+                for(int i=0; i<= Y; i++)
+                {
+                    //GameObject go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                    GameObject go = Instantiate(wallPrefab);
+                    go.transform.localScale = new Vector3(mSceneRect.width, 20, 0.01f);
+                    go.transform.position = new Vector3(mSceneRect.x + mSceneRect.width / 2.0f, 10, mSceneRect.y + i * ySkip);
+                    go.transform.parent = YObjs.transform;
+                }
+
+            }
+            
+            if(X != gridX || Y != gridY || mSceneRect.x != sceneRegion.x || mSceneRect.y != sceneRegion.y 
+                || mSceneRect.width != sceneRegion.width || mSceneRect.height != sceneRegion.height)
+            {
+
+                X = gridX; Y = gridY;
+                mSceneRect.x = sceneRegion.x;  mSceneRect.y = sceneRegion.y;
+                mSceneRect.width = sceneRegion.width; mSceneRect.height = sceneRegion.height;
+
+                GameObject.DestroyImmediate(mObjectGroup);
+                mObjectGroup = null;
+            }
+
+        }
+        else
+        {
+
+            if(mObjectGroup != null)
+            {
+                GameObject.DestroyImmediate(mObjectGroup);
+                mObjectGroup = null;
+            }
+           
+
+        }
+
+
+    }
+}
--- Assets/Scripts/TerrainGrid.cs
+++ Assets/Scripts/TerrainGrid.cs
@@ -22,7 +22,8 @@ public class TerrainGrid : MonoBehaviour {
 
     public TileNum Grid_NxN;
     public bool 暂停更新 = false; //暂停更新
-    private TerrainGridCoreDLL terrainGridCore = null;
+    public bool 显示编号 = false;
+    public TerrainGridCoreDLL terrainGridCore = null;
 
     TerrainGrid()
     {
@@ -49,8 +50,7 @@ public class TerrainGrid : MonoBehaviour {
         ms.receiveShadows = false;
 
         EditorApplication.update += MyUpdate;
-
-        Debug.Log("Start");
+        
 	}
 	
 	// Update is called once per frame
@@ -65,7 +65,7 @@ public class TerrainGrid : MonoBehaviour {
     {
         if (!暂停更新)
         {
-            terrainGridCore.MyUpdate((int)Grid_NxN);
+            terrainGridCore.MyUpdate((int)Grid_NxN);           
         }
     }
 
@@ -76,17 +76,19 @@ public class TerrainGrid : MonoBehaviour {
         {
             terrainGridCore.showErrorWindow = false;
             Rect wr = new Rect(0, 0, 500, 500);
-            EditorGUI.HelpBox(wr, "警告:该地形块内的贴图数目大于5张，超出5张的贴图将无法在游戏里显示\n请将地形划分成更多的网格来解决此问题", MessageType.Error);                                
+            EditorGUI.HelpBox(wr, "警告:该地形块内的贴图数目大于5张，超出5张的贴图将无法在游戏里显示\n请将地形划分成更多的网格来解决此问题", MessageType.Error);
+
+
+            Rect wr2 = new Rect(0, 0, 500, 500);
+            // EditorGUI.HelpBox(wr, "警告:该地形块内的贴图数目大于5张，超出5张的贴图将无法在游戏里显示\n请将地形划分成更多的网格来解决此问题", MessageType.Error);                                
+            GUIContent[] opts = new GUIContent[1];
+            opts[0] = new GUIContent("警告:该地形块内的贴图数目大于5张，超出5张的贴图将无法在游戏里显示\n请将地形划分成更多的网格来解决此问题");
+            EditorGUI.Popup(wr2, 0, opts);
+        
         }
 
-        Rect wr2 = new Rect(0, 0, 500, 500);
-        // EditorGUI.HelpBox(wr, "警告:该地形块内的贴图数目大于5张，超出5张的贴图将无法在游戏里显示\n请将地形划分成更多的网格来解决此问题", MessageType.Error);                                
-        GUIContent[] opts = new GUIContent[1];
-        opts[0] = new GUIContent("警告:该地形块内的贴图数目大于5张，超出5张的贴图将无法在游戏里显示\n请将地形划分成更多的网格来解决此问题");
-        EditorGUI.Popup(wr2, 0, opts);
+        Debug.LogError("OnGUI");
 
-
-        Debug.Log("OnGUI");
     }
 
 
@@ -98,28 +100,72 @@ public class TerrainGrid : MonoBehaviour {
 
 
 [CustomEditor( typeof( TerrainGrid ) )]
+
+
 public class DrawLineEditor : Editor
 {
     // draw lines between a chosen game object
     // and a selection of added game objects
 
-    public void OnGUI( )
-    {
-        Debug.Log("DrawLineEditor");
 
+    TerrainGrid mTerrainGrid = null;
+    Terrain mTerrain = null;
+
+    public DrawLineEditor()
+    {
+
+    }
+
+    public void OnSceneGUI()
+    {
+       
+        
         // get the chosen game object
         TerrainGrid t = target as TerrainGrid;
-
         if( t == null)
             return;
+   
+        if (mTerrainGrid == null)
+        {
+            foreach (GameObject obj in Object.FindObjectsOfType(typeof(GameObject)))
+            {
+                Terrain terrain = obj.GetComponent<Terrain>();
+                if (terrain != null)
+                {
+                    mTerrainGrid = obj.GetComponent<TerrainGrid>();
+                    mTerrain = terrain;
+                    break;
+                }          
+            }
+        }
+
+        if (mTerrainGrid == null) return;
+
+        if (mTerrainGrid.显示编号 == false) return;
+
+         
+        int grids = (int)mTerrainGrid.Grid_NxN;
+        grids = (int)System.Math.Pow(2, grids);
+        float skipX = mTerrain.terrainData.size.x / grids;
+        float skipY = mTerrain.terrainData.size.z / grids;
+
+        for(int y = 0; y< grids; y++)
+        {
+            for(int x = 0; x< grids; x++)
+            {
+                int terrainNumber = y * grids + x;
+                Vector3 center = new Vector3(t.transform.position.x + x * skipX + skipX / 2.0f, 0, t.transform.position.z + y * skipY + skipY / 2.0f);
+                Handles.DrawLine(center, new Vector3(center.x, 100, center.z));
+                Handles.Label(new Vector3(center.x, 103, center.z), terrainNumber.ToString());
+            }
+
+        }
+
+
 
         // grab the center of the parent
-        Vector3 center = t.transform.position;
-        Handles.DrawLine(new Vector3(0, 0, 0), new Vector3(100, 200, 200));
-        Rect wr = new Rect(0, 0, 500, 500);
-       // EditorGUI.HelpBox(wr, "警告:该地形块内的贴图数目大于5张，超出5张的贴图将无法在游戏里显示\n请将地形划分成更多的网格来解决此问题", MessageType.Error);                                
-       GUIContent[] opts = new GUIContent[1];
-        opts[0] = new GUIContent("警告:该地形块内的贴图数目大于5张，超出5张的贴图将无法在游戏里显示\n请将地形划分成更多的网格来解决此问题");
-        EditorGUI.Popup(wr, 0, opts);
+       // Vector3 center = t.transform.position;
+        //Handles.DrawLine(new Vector3(0, 0, 0), new Vector3(100, 200, 200));
+       // Handles.Label(new Vector3(100, 200, 200), "2");
     }
 }
